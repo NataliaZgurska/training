ed3dec38ad296e93ea661df137ffaed0d956969c
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-02-19 07:22:12 +0200

    update

---
+++ src/js/task-n.js
@@ -0,0 +1,14 @@
+// function makeSizer(size) {
+//   return function() {
+//     document.body.style.fontSize = size + 'px';
+//   };
+// }
+
+// const size10 = makeSizer(10);
+// const size14 = makeSizer(14);
+// const size20 = makeSizer(20);
+
+
+// document.getElementById('size-10').onclick = size10;
+// document.getElementById('size-14').onclick = size14;
+// document.getElementById('size-20').onclick = size20;
